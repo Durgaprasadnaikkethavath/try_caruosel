ac07c8ecc7d482688d5f139ef5da5a1b5b9a7bb6
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-20 22:27:25 +0530

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ImageCarousel from "./Components/ImageCarousel";
 
 const App = () => {
   return (
     <>
       <h1>App Carousel</h1>
-      <ImageCarousel />
     </>
   );
 };
