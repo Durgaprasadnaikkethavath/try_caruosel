cd11d50a18d2fb426cc3edf607158a4283264bdc
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-21 16:06:13 +0530

    updated Images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ImageCarousel from "./Components/ImageCarousel";
+// import ImageCarousel from "./Components/ImageCarousel";
 
 const App = () => {
   return (
     <>
+      <h1>Given to Carousel Images are given to error </h1>
       <h1>App Carousel</h1>
-      <ImageCarousel />
     </>
   );
 };
