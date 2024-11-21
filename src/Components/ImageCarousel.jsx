024fa3760034d18b636be3ba9dbcfdfd57ba1f87
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-11-21 16:09:56 +0530

    next some

--- src/Components/ImageCarousel.jsx
+++ src/Components/ImageCarousel.jsx
@@ -4,27 +4,25 @@ import React from "react";
 const ImageCarousel = () => {
   return (
     <div>
+      <p>Updated Carousel Images</p>
       <h1>Carousel</h1>
       <Carousel>
         <Carousel.Item>
           <img
             src="https://picsum.photos/800/600?random=1"
-            width="100%"
-            height="400vh"
+            alt="carousel images"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            width="100%"
             src="https://picsum.photos/800/600?random=2"
-            height="400vh"
+            alt="carousel images"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            width="100%"
             src="https://picsum.photos/800/600?random=3"
-            height="400vh"
+            alt="carousel images"
           />
         </Carousel.Item>
       </Carousel>
